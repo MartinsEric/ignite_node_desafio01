23c083a4202de68a09e91e5e89c82766a7d2276d
Eric Martins <eric.martins@radixeng.com.br>
2021-03-09 20:27:29 -0300

    route to delete a todo

--- src/index.js
+++ src/index.js
@@ -100,7 +100,18 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  
+  const { user } = request;
+  const { id } = request.params;
+
+  const todoIndex = user.todos.findIndex(todo => todo.id === id);
+
+  if(todoIndex === -1) {
+    return response.status(404).json({ error: 'Todo not found!' });
+  }
+
+  user.todos.splice(todoIndex, 1);
+
+  return response.status(204).send();
 });
 
 module.exports = app;
